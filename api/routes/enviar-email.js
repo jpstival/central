9f521b5f630ed0346b5d66d240707b47528da73f
João Paulo Stival <Joo Paulo Stival>
2017-07-03 07:41:52 -0300

    commit

--- api/routes/enviar-email.js
+++ api/routes/enviar-email.js
@@ -4,7 +4,7 @@ var sendMailService   = require('../services/nodemailer/sendMail-service');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    console.log(req.body);
+
     config = {
         host: 'smtp.gmail.com', 
         port: 465,
@@ -21,35 +21,37 @@ router.get('/', function(req, res, next) {
         html: '<b>Hello world ?</b>'
     }
 
-    sendMailService.enviar(config, dados);
+    // sendMailService.enviar(config, dados);
     res.send('respond with a email');
 
 })
 
 router.post('/', function(req, res, next) {
 
-    config = req.header('config');
-    console.log('entrou');
-    console.log(config);
-    dados = req.header('dados');
-    console.log(dados);
-    // &config = '{'
-    // &config += "host: 'smtp.gmail.com', "
-    // &config += 'port: 465, '
-    // &config += 'secure: true, '
-    // &config += "email: 'joaopaulosstival@gmail.com', "
-    // &config += "senha: '986stival' "
-    // &config += '}'
+// {
+// 	"config": {
+// 		"host": "smtp.gmail.com",
+// 		"port": "465",
+// 		"secure": "true",
+// 		"email": "joaopaulosstival@gmail.com",
+// 		"senha": "986stival"
+// 	},
+// 	"dados": {
+// 		"from": "joaopaulosstival@live.com",
+// 		"to": "joaopaulosstival@gmail.com",
+// 		"subject": "olá mundo",
+// 		"text": "fala ai mano",
+// 		"html": "<b>Hello world ?</b>"
+// 	}
+// }
+    requisicao = req.body; 
+    console.log(requisicao);
+    // config = requisicao.config;
+    // console.log(config);
+    // dados = requisicao.dados;
+    // console.log(dados);
 
-    // &dados = '{'
-    // &dados += "from: 'joaopaulosstival@live.com', "
-    // &dados += "to: 'joaopaulosstival@gmail.com', "
-    // &dados += "subject: 'olá mundo', "
-    // &dados += "text: 'fala ai mano', "
-    // &dados += "html: '<b>Hello world ?</b>' "
-    // &dados += '}'
-
-    sendMailService.enviar(config, dados);
+    // sendMailService.enviar(config, dados);
     res.send('respond with a email');
 
 })
--- api/routes/index.js
+++ api/routes/index.js
@@ -5,87 +5,90 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  const nodemailer = require('nodemailer');
+//   const nodemailer = require('nodemailer');
 
-  // create reusable transporter object using the default SMTP transport
-  let transporter = nodemailer.createTransport({
-      host: 'smtp.gmail.com',
-      port: 465,
-      secure: true, // secure:true for port 465, secure:false for port 587
-      auth: {
-          user: 'joaopaulosstival@gmail.com',
-          pass: '986stival'
-      }
-  });
-
-    // var mammoth = require("mammoth");
+var toPdf = require("office-to-pdf")
  
-    // mammoth.convertToHtml({path: "./DIFACT.docx"})
-    // .then(function(result){
-    //     var html = result.value; // The generated HTML 
-    //     //console.log(html);
-    //    var messages = result.messages; // Any messages, such as warnings during conversion 
+// create reusable transporter object using the default SMTP transport
+//   let transporter = nodemailer.createTransport({
+//       host: 'smtp.gmail.com',
+//       port: 465,
+//       secure: true, // secure:true for port 465, secure:false for port 587
+//       auth: {
+//           user: 'joaopaulosstival@gmail.com',
+//           pass: '986stival'
+//       }
+//   });
 
-    // var docxConverter = require('docx-pdf');
+
+//     // var mammoth = require("mammoth");
+ 
+//     // mammoth.convertToHtml({path: "./DIFACT.docx"})
+//     // .then(function(result){
+//     //     var html = result.value; // The generated HTML 
+//     //     //console.log(html);
+//     //    var messages = result.messages; // Any messages, such as warnings during conversion 
+
+//     // var docxConverter = require('docx-pdf');
     
-    // docxConverter('./DIFACT.docx','./DIFACT.pdf',function(err,result){
-    // if(err){
-    //     console.log(err);
-    // }
-    // console.log('result'+result);
-    // });
-  // setup email data with unicode symbols
-  let mailOptions = {
-      from: '"Fred Foo 👻" <joaopaulosstival@live.com>', // sender address
-      to: 'joaopaulosstival@gmail.com', // list of receivers
-      subject: 'Hello ✔', // Subject line
-      text: 'Hello world ?' // plain text body
-      //html: '<b>Hello world ?</b>' // html body
-          //attachments: [
-            // {   // binary buffer as an attachment
-            //     filename: 'text2.txt',
-            //     content: new Buffer('hello world!','utf-8')
-            // },
-            // {   // file on disk as an attachment
-            //     filename: 'difact.pdf',
-            //     path: 'DIFACT.pdf' // stream this file
-            // },
-            // {   // filename and content type is derived from path
-            //     path: '/path/to/file.txt'
-            //},
-            // {   // stream as an attachment
-            //     filename: 'text4.txt',
-            //     content: fs.createReadStream('file.txt')
-            // },
-            // {   // define custom content type for the attachment
-            //     filename: 'text.bin',
-            //     content: 'hello world!',
-            //     contentType: 'text/plain'
-            // },
-            // {   // define custom content type for the attachment
-            //     filename: 'text.html',
-            //     content: html,
-            //     contentType: 'text/html'
-            // },
-            // {   // use URL as an attachment
-            //     filename: 'license.txt',
-            //     path: 'https://raw.github.com/nodemailer/nodemailer/master/LICENSE'
-            // },
-            // {   // encoded string as an attachment
-            //     filename: 'text1.txt',
-            //     content: 'aGVsbG8gd29ybGQh',
-            //     encoding: 'base64'
-            // },
-       // ]
-  };
+//     // docxConverter('./DIFACT.docx','./DIFACT.pdf',function(err,result){
+//     // if(err){
+//     //     console.log(err);
+//     // }
+//     // console.log('result'+result);
+//     // });
+//   // setup email data with unicode symbols
+//   let mailOptions = {
+//       from: '"Fred Foo 👻" <joaopaulosstival@live.com>', // sender address
+//       to: 'joaopaulosstival@gmail.com', // list of receivers
+//       subject: 'Hello ✔', // Subject line
+//       text: 'Hello world ?' // plain text body
+//       //html: '<b>Hello world ?</b>' // html body
+//           //attachments: [
+//             // {   // binary buffer as an attachment
+//             //     filename: 'text2.txt',
+//             //     content: new Buffer('hello world!','utf-8')
+//             // },
+//             // {   // file on disk as an attachment
+//             //     filename: 'difact.pdf',
+//             //     path: 'DIFACT.pdf' // stream this file
+//             // },
+//             // {   // filename and content type is derived from path
+//             //     path: '/path/to/file.txt'
+//             //},
+//             // {   // stream as an attachment
+//             //     filename: 'text4.txt',
+//             //     content: fs.createReadStream('file.txt')
+//             // },
+//             // {   // define custom content type for the attachment
+//             //     filename: 'text.bin',
+//             //     content: 'hello world!',
+//             //     contentType: 'text/plain'
+//             // },
+//             // {   // define custom content type for the attachment
+//             //     filename: 'text.html',
+//             //     content: html,
+//             //     contentType: 'text/html'
+//             // },
+//             // {   // use URL as an attachment
+//             //     filename: 'license.txt',
+//             //     path: 'https://raw.github.com/nodemailer/nodemailer/master/LICENSE'
+//             // },
+//             // {   // encoded string as an attachment
+//             //     filename: 'text1.txt',
+//             //     content: 'aGVsbG8gd29ybGQh',
+//             //     encoding: 'base64'
+//             // },
+//        // ]
+//   };
 
   // send mail with defined transport object
-  transporter.sendMail(mailOptions, (error, info) => {
-      if (error) {
-          return console.log(error);
-      }
-      console.log('Message %s sent: %s', info.messageId, info.response);
-  });
+//   transporter.sendMail(mailOptions, (error, info) => {
+//       if (error) {
+//           return console.log(error);
+//       }
+//       console.log('Message %s sent: %s', info.messageId, info.response);
+//   });
   res.render('index', { title: 'Express' });
 //   })
 // .done();
